35c32738127ea1033780e67e5d7672af6ba37f5e
cedric karungu <ckarungu921@gmail.com>
2022-10-03 14:31:49 +0200

    feat: add many state and actions

--- js_redux/src/index.js
+++ js_redux/src/index.js
@@ -1,5 +1,5 @@
-console.log('welcome');
 const BUY_PHONE = 'BUY_PHONE';
+const BUY_TAB = 'BUY_TAB';
 
 const buyPhone = () => {
 	// Action
@@ -7,20 +7,28 @@ const buyPhone = () => {
 		type: BUY_PHONE,
 	};
 };
+const buyTab = () => {
+	// Action
+	return {
+		type: BUY_TAB,
+	};
+};
 
 // Reducer
-
-// (prevState, action) => newState
+// _______
 
 // init states
 const initialState = {
-	phones: 10,
+	phones: 5,
+	tablettes: 10,
 };
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case BUY_PHONE:
 			return { ...state, phones: state.phones - 1 };
+		case BUY_TAB:
+			return { ...state, tablettes: state.tablettes - 1 };
 
 		default:
 			return state;
@@ -32,18 +40,34 @@ const reducer = (state = initialState, action) => {
 // getState pour recuperer la data
 // dispatch effectuer une action
 
+//  attacher le store au reducer
+// _____________________________
+
 const store = Redux.createStore(reducer);
 
-const availablePhones = document.querySelector('#count');
+const availablePhones = document.querySelector('#count-phone');
 availablePhones.innerHTML = store.getState().phones;
+
+const availableTablettes = document.querySelector('#count-tab');
+availableTablettes.innerHTML = store.getState().tablettes;
+
+//dispacther les actions
+// _____________________
 
 document.querySelector('#buy-phone').addEventListener('click', () => {
 	//permettre de modifier un state via l'action se trouvant dans buyPhone
 	store.dispatch(buyPhone());
+	// store.dispatch({ type: BUY_PHONE });  //alternative
+});
+document.querySelector('#buy-tab').addEventListener('click', () => {
+	//permettre de modifier un state via l'action se trouvant dans buyPhone
+	store.dispatch(buyTab());
+	// store.dispatch({ type: BUY_TAB }); //alternative
 });
 
-//ecouter si le state a ete modifier
+//ecouter si le state a ete modifier et update le dom
+// _________________________________
 store.subscribe(() => {
-	console.log(store.getState());
 	availablePhones.innerHTML = store.getState().phones;
+	availableTablettes.innerHTML = store.getState().tablettes;
 });
